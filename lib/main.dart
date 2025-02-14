3b434e17a031d36d11e611da3c08d6fe3b508c73
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-14 18:45:51 -0500

    Modify main for implement bloc provider and register routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,35 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:double_partner_test/presentation/auth/views/login_screen.dart';
+import 'package:double_partner_test/presentation/blocs/auth/auth_bloc.dart';
+import 'package:double_partner_test/presentation/pages/register_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'core/injection/injection_container.dart' as di;
+import 'package:double_partner_test/presentation/pages/login_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await di.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Double partner Test',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginView()
-      },
-    );
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => di.sl<AuthBloc>())
+      ],
+      child: MaterialApp(
+        title: 'Double partner Test',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginPage(),
+          '/register': (context) => RegisterView()
+        },
+      )
+      );
   }
 }
