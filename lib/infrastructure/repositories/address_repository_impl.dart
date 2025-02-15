13ca768348f5a401112f7932383aaa6a032011e8
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-15 11:44:55 -0500

    Create repository for managemente data

---
+++ lib/infrastructure/repositories/address_repository_impl.dart
@@ -0,0 +1,41 @@
+import 'package:dartz/dartz.dart';
+import 'package:double_partner_test/domain/entities/address.dart';
+import 'package:double_partner_test/domain/repositories/i_address_repository.dart';
+import 'package:double_partner_test/infrastructure/datasources/firebase_addresses_datasource.dart';
+import 'package:double_partner_test/infrastructure/models/address_model.dart';
+import '../../core/errors/failures.dart';
+import '../../core/errors/exceptions.dart';
+import '../models/user_model.dart';
+
+class AddressRepositoryImpl implements IAddressRepository {
+  final FirebaseAddressDatasource _addressDatasource;
+
+  AddressRepositoryImpl(this._addressDatasource);
+
+  @override
+  Future<Either<Failure, Address>> create(Address data, String userId) async {
+    try {
+      final addressData = await _addressDatasource.createAddress(
+          AddressModel.fromDomain(data).toMap(), userId);
+      final addressCreated = AddressModel.fromMap(addressData).toDomain();
+      return Right(addressCreated);
+    } on AuthException catch (e) {
+      return Left(AuthFailure(e.message));
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Address>>> getAll() async {
+    try {
+      final userData = await _addressDatasource.getAllAddresses();
+      return Right(userData);
+    } on AuthException catch (e) {
+      return Left(AuthFailure(e.message));
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.message));
+    }
+  }
+
+}
