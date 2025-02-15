68ba7b31431e272e200fcaea5ca293bf0889a1f9
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-15 09:53:20 -0500

    Create entity for address

---
+++ lib/domain/entities/address.dart
@@ -0,0 +1,18 @@
+class Address {
+  final String id;
+  final String userId;
+  final String street;
+  final String city;
+  final String state;
+
+  Address({
+    required this.id,
+    required this.userId,
+    required this.street,
+    required this.city,
+    required this.state,
+  });
+
+  @override
+  List<Object> get props  => [id, userId, street, city, state];
+}
