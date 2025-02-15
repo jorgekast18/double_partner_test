cbe1f91c8eecfb22a9e1486a0c506a0e21e29d17
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-15 10:54:40 -0500

    Modify use cases create and get all addresses for add logic if user is authenticad

--- lib/domain/usecases/create_address.dart
+++ lib/domain/usecases/create_address.dart
@@ -1,5 +1,6 @@
 import 'package:double_partner_test/domain/entities/address.dart';
 import 'package:double_partner_test/domain/repositories/i_address_repository.dart';
+import 'package:double_partner_test/domain/usecases/authenticated.dart';
 import 'package:equatable/equatable.dart';
 import 'package:dartz/dartz.dart';
 import 'package:double_partner_test/core/errors/failures.dart';
@@ -7,12 +8,27 @@ import 'package:double_partner_test/core/usecases/usecase.dart';
 
 class CreateAddress implements UseCase<Address, CreateAddressParams> {
   final IAddressRepository repository;
+  final IsAuth _isAuth;
 
-  CreateAddress(this.repository);
+  CreateAddress(this.repository, this._isAuth);
 
   @override
   Future<Either<Failure, Address>> call(CreateAddressParams params) async {
-    return await repository.create(params.address);
+
+    final authStatus = await _isAuth();
+
+    return authStatus.fold(
+        (failure) {
+          return const  Left(ServerFailure('Error al verificar el estado de autenticación.'));
+        }, (isAuthenticated) async {
+          if(isAuthenticated){
+            return await repository.create(params.address);
+          } else {
+            return const Left(AuthFailure('El usuario no está autenticado'));
+          }
+
+      }
+    );
   }
 }
 
--- lib/domain/usecases/get_addresses.dart
+++ lib/domain/usecases/get_addresses.dart
@@ -3,15 +3,30 @@ import 'package:double_partner_test/core/errors/failures.dart';
 import 'package:double_partner_test/core/usecases/usecase.dart';
 import 'package:double_partner_test/domain/entities/address.dart';
 import 'package:double_partner_test/domain/repositories/i_address_repository.dart';
+import 'package:double_partner_test/domain/usecases/authenticated.dart';
 
 class GetAllAddresses implements NoParamsUseCase<List<Address>> {
   final IAddressRepository repository;
+  final IsAuth _isAuth;
 
-  GetAllAddresses(this.repository);
+  GetAllAddresses(this.repository, this._isAuth);
 
   @override
   Future<Either<Failure, List<Address>>> call() async {
-      final result = await repository.getAll();
-      return Right(result as List<Address>);
+      final authStatus = await _isAuth();
+
+      return authStatus.fold(
+          (failure) {
+            return const  Left(ServerFailure('Error al verificar el estado de autenticación.'));
+          }, (isAuthenticated) async {
+        if(isAuthenticated){
+          final result = await repository.getAll();
+          return Right(result as List<Address>);
+        } else {
+          return const Left(AuthFailure('El usuario no está autenticado'));
+        }
+
+      }
+      );
   }
 }
