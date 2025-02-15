5a494b900a127c1be6ed41afe81dcfdf4bf1c47f
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-15 09:53:42 -0500

    Create repository interface for address

---
+++ lib/domain/repositories/i_address_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:double_partner_test/domain/entities/address.dart';
+
+import '../../core/errors/failures.dart';
+
+abstract class IAddressRepository {
+  Future<Either<Failure, Address>> create(Address address);
+  Future<Either<Failure, Address>> getAll(List<Address> address);
+}
