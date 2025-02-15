a4c8df8666b58c9b13081b77ae4befdeb43446fd
Jorge Castaño <jorgecastano@MacBook-Air-de-Jorge.local>
2025-02-15 15:11:17 -0500

    add bloc address for inject multi provider bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:double_partner_test/presentation/blocs/addresses/address_bloc.dart';
 import 'package:double_partner_test/presentation/blocs/auth/auth_bloc.dart';
-import 'package:double_partner_test/presentation/pages/home_view.dart';
-import 'package:double_partner_test/presentation/pages/register_view.dart';
+import 'package:double_partner_test/presentation/pages/home/home_view.dart';
+import 'package:double_partner_test/presentation/pages/login/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'core/injection/injection_container.dart' as di;
-import 'package:double_partner_test/presentation/pages/login_view.dart';
+import 'package:double_partner_test/presentation/pages/login/login_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 
@@ -21,16 +22,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => di.sl<AuthBloc>())
+        BlocProvider(create: (_) => di.sl<AuthBloc>()),
+        BlocProvider(create: (_) => di.sl<AddressBloc>())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Double partner Test',
         initialRoute: '/',
         routes: {
-          '/': (context) => LoginPage(),
+          '/': (context) => const LoginPage(),
           '/register': (context) => RegisterView(),
-          '/home': (context) => HomePage()
+          '/home': (context) => const HomePage()
         },
       )
       );
